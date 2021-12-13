a5d4c4a20ae6efdc40348fb2615e9d9d7a4572ce
Rizwan Akram <rizwanakram4049@gmail.com>
2021-12-14 05:19:17 +0530

    Adds url and file loader for images

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import nav from "./nav";
 import "./footer.css";
 import "./button.css";
+import imageUrl from "./webpack-logo.jpeg";
+import { makeImage } from "./image";
 
 import { footer } from "./footer";
 import makeButton from "./button";
 import { makeColorStyle } from "./button-styles";
 
+const image = makeImage(imageUrl);
+
 let botton = makeButton("Yay this is  button");
 botton.style = makeColorStyle("red");
 
 document.body.appendChild(botton);
-
+document.body.appendChild(image);
 document.body.appendChild(footer);
